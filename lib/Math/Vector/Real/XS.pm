3f35d12d35119fab3ae38f4d48329aeed367c02c
Salvador <sfandino@yahoo.com>
2012-07-17 14:36:57 +0200

    add support section into the docs

--- lib/Math/Vector/Real/XS.pm
+++ lib/Math/Vector/Real/XS.pm
@@ -29,6 +29,20 @@ L<Math::Vector::Real> in XS for a great performance boost.
 Once this module is installed, L<Math::Vector::Real> will load and use
 it automatically.
 
+=head1 SUPPORT
+
+In order to report bugs you can send me and email to the address that
+appears below or use the CPAN RT bugtracking system available at
+L<http://rt.cpan.org>.
+
+The source for the development version of the module is hosted at
+GitHub: L<https://github.com/salva/p5-Math-Vector-Real-XS>.
+
+=head2 My wishlist
+
+If you like this module and you're feeling generous, take a look at my
+Amazon Wish List: L<http://amzn.com/w/1WU1P6IR5QZ42>
+
 =head1 COPYRIGHT AND LICENSE
 
 Copyright (C) 2012 by Salvador Fandiño (sfandino@yahoo.com).
